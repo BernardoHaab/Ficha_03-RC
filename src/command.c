7a00bdd7a12f90bd28c185f25be7cfec82274736
ShellTux <115948079+ShellTux@users.noreply.github.com>
2024-03-16 21:46:00 +0000

    fix(command.c): when parsing the buffer trim leading/trailing whitespaces

--- src/command.c
+++ src/command.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,6 +9,46 @@
 #define DELIMITER " "
 #define DOMAIN_FILE_DELIMITER " "
 
+char *trim(char *string)
+{
+	size_t length = 0;
+	char *frontP = string;
+	char *endP = NULL;
+
+	if( string == NULL ) { return NULL; }
+	if( string[0] == '\0' ) { return string; }
+
+	length = strlen(string);
+	endP = string + length;
+
+	/* Move the front and back pointers to address the first non-whitespace
+	 * characters from each end.
+	 */
+	while( isspace((unsigned char) *frontP) ) { ++frontP; }
+	if( endP != frontP )
+	{
+		while( isspace((unsigned char) *(--endP)) && endP != frontP ) {}
+	}
+
+	if( frontP != string && endP == frontP )
+		*string = '\0';
+	else if( string + length - 1 != endP )
+		*(endP + 1) = '\0';
+
+	/* Shift the string so that it starts at str so that if it's dynamically
+	 * allocated, we can still free it on the returned pointer.  Note the reuse
+	 * of endp to mean the front of the string buffer now.
+	 */
+	endP = string;
+	if( frontP != string )
+	{
+		while( *frontP ) { *endP++ = *frontP++; }
+		*endP = '\0';
+	}
+
+	return string;
+}
+
 Command processCommand(
 		const char *const buffer,
 		const size_t bufferSize,
@@ -15,9 +56,10 @@ Command processCommand(
 		)
 {
 	char *bufferClone = malloc(sizeof(buffer[0]) * bufferSize);
-	char *bufferCloneFree = bufferClone;
 
 	strncpy(bufferClone, buffer, bufferSize);
+
+	char *bufferCloneFree = trim(bufferClone);
 
 	char *commandString = strtok(bufferClone, DELIMITER);
 	char *argumentString = strtok(NULL, DELIMITER);
