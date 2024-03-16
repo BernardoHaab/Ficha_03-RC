5a2fe36a127d591a29f7a43e956f4923afb1dbfa
ShellTux <115948079+ShellTux@users.noreply.github.com>
2024-03-16 23:44:13 +0000

    style: indent code with tabs

--- src/udp-client.c
+++ src/udp-client.c
@@ -15,8 +15,8 @@
 
 void erro(char *s)
 {
-  perror(s);
-  exit(1);
+	perror(s);
+	exit(1);
 }
 
 void usage(const char *const programName)
@@ -31,48 +31,48 @@ void usage(const char *const programName)
 
 int main(int argc, char *argv[])
 {
-  struct sockaddr_in si_res, si_outra;
+	struct sockaddr_in si_res, si_outra;
 
-  int socketFD, receivedBytes;
-  socklen_t slen = sizeof(si_outra);
-  char buffer[BUFFER_SIZE];
+	int socketFD, receivedBytes;
+	socklen_t slen = sizeof(si_outra);
+	char buffer[BUFFER_SIZE];
 
-  if (argc != 4) {
-	  usage(argv[0]);
-  }
+	if (argc != 4) {
+		usage(argv[0]);
+	}
 
-  // Cria um socket para envio e recepção de pacotes UDP
-  if ((socketFD = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
-	  error("Erro na criação do socket");
-  }
+	// Cria um socket para envio e recepção de pacotes UDP
+	if ((socketFD = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
+		error("Erro na criação do socket");
+	}
 
-  debugMessage(stdout, OK, "UDP Socket successfully created!\n");
+	debugMessage(stdout, OK, "UDP Socket successfully created!\n");
 
-  // Preenchimento da socket address structure
-  si_outra.sin_family = AF_INET;
-  si_outra.sin_addr.s_addr = inet_addr(argv[1]);
-  si_outra.sin_port = htons(atoi(argv[2]));
+	// Preenchimento da socket address structure
+	si_outra.sin_family = AF_INET;
+	si_outra.sin_addr.s_addr = inet_addr(argv[1]);
+	si_outra.sin_port = htons(atoi(argv[2]));
 
-  char number[NUMBER_MAX_LENGTH];
+	char number[NUMBER_MAX_LENGTH];
 
-  strncpy(number, argv[3], NUMBER_MAX_LENGTH);
-  printf("Numero: %s\n", number);
+	strncpy(number, argv[3], NUMBER_MAX_LENGTH);
+	printf("Numero: %s\n", number);
 
-  if (sendto(socketFD, number, strlen(number), 0, (struct sockaddr *)&si_outra, slen) < 0) {
-    error("Erro no sendto");
-  }
+	if (sendto(socketFD, number, strlen(number), 0, (struct sockaddr *)&si_outra, slen) < 0) {
+		error("Erro no sendto");
+	}
 
-  // Espera recepção de mensagem (a chamada é bloqueante)
-  if ((receivedBytes = recvfrom(socketFD, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&si_res, (socklen_t *)&slen)) == -1)
-  {
-    error("Erro no recvfrom");
-  }
-  // Para ignorar o restante conteúdo (anterior do buffer)
-  buffer[receivedBytes] = '\0';
+	// Espera recepção de mensagem (a chamada é bloqueante)
+	if ((receivedBytes = recvfrom(socketFD, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&si_res, (socklen_t *)&slen)) == -1)
+	{
+		error("Erro no recvfrom");
+	}
+	// Para ignorar o restante conteúdo (anterior do buffer)
+	buffer[receivedBytes] = '\0';
 
-  printf("Resposta:\n%s", buffer);
+	printf("Resposta:\n%s", buffer);
 
-  // Fecha socket e termina programa
-  close(socketFD);
-  return 0;
+	// Fecha socket e termina programa
+	close(socketFD);
+	return 0;
 }
--- src/udp-server.c
+++ src/udp-server.c
@@ -17,18 +17,18 @@
 
 void erro(char *s)
 {
-  perror(s);
-  exit(1);
+	perror(s);
+	exit(1);
 }
 
 void usage(const char *const programName) {
-    printf("Usage: %s [options]\n", programName);
-    printf("Options:\n");
-    printf("  -a, --address <ip address>   Set the server IP address\n");
-    printf("  -p, --port <port>            Set the server port number\n");
-    printf("  -h, --help                   Print this usage message\n");
+	printf("Usage: %s [options]\n", programName);
+	printf("Options:\n");
+	printf("  -a, --address <ip address>   Set the server IP address\n");
+	printf("  -p, --port <port>            Set the server port number\n");
+	printf("  -h, --help                   Print this usage message\n");
 
-    exit(EXIT_FAILURE);
+	exit(EXIT_FAILURE);
 }
 
 int main(int argc, char **argv)
@@ -71,95 +71,95 @@ int main(int argc, char **argv)
 		}
 	}
 
-  struct sockaddr_in si_minha, si_outra;
+	struct sockaddr_in si_minha, si_outra;
 
-  int socketFD, receivedBytes;
-  socklen_t slen = sizeof(si_outra);
-  char buffer[BUFFER_SIZE];
+	int socketFD, receivedBytes;
+	socklen_t slen = sizeof(si_outra);
+	char buffer[BUFFER_SIZE];
 
-  // Cria um socket para recepção de pacotes UDP
-  if ((socketFD = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
-    error("Erro na criação do socket");
-  }
+	// Cria um socket para recepção de pacotes UDP
+	if ((socketFD = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
+		error("Erro na criação do socket");
+	}
 
-  debugMessage(stdout, OK, "UDP Socket successfully opened!\n");
+	debugMessage(stdout, OK, "UDP Socket successfully opened!\n");
 
-  // Preenchimento da socket address structure
-  si_minha.sin_family      = AF_INET;
-  si_minha.sin_port        = htons(port);
-  si_minha.sin_addr.s_addr = inet_addr(ipAddress);
+	// Preenchimento da socket address structure
+	si_minha.sin_family      = AF_INET;
+	si_minha.sin_port        = htons(port);
+	si_minha.sin_addr.s_addr = inet_addr(ipAddress);
 
-  // Associa o socket à informação de endereço
-  if (bind(socketFD, (struct sockaddr *)&si_minha, sizeof(si_minha)) == -1)
-  {
-	  error("Error binding socket\n");
-  }
+	// Associa o socket à informação de endereço
+	if (bind(socketFD, (struct sockaddr *)&si_minha, sizeof(si_minha)) == -1)
+	{
+		error("Error binding socket\n");
+	}
 
-  char ipAddressString[INET_ADDRSTRLEN] = {0};
-  debugMessage(stdout, OK, "Successfully binded to "
-  		RED "%s" RESET ":" GREEN "%d" RESET "\n",
-  		inet_ntop(
-  			AF_INET,
-  			&si_minha.sin_addr,
-  			ipAddressString,
-  			INET_ADDRSTRLEN
-  			),
-  		ntohs(si_minha.sin_port));
-  debugMessage(stdout, INFO, "Listening for connections...\n");
+	char ipAddressString[INET_ADDRSTRLEN] = {0};
+	debugMessage(stdout, OK, "Successfully binded to "
+			RED "%s" RESET ":" GREEN "%d" RESET "\n",
+			inet_ntop(
+				AF_INET,
+				&si_minha.sin_addr,
+				ipAddressString,
+				INET_ADDRSTRLEN
+				),
+			ntohs(si_minha.sin_port));
+	debugMessage(stdout, INFO, "Listening for connections...\n");
 
-  do {
-	  // Espera recepção de mensagem (a chamada é bloqueante)
-	  if ((receivedBytes = recvfrom(
-					  socketFD,
-					  buffer,
-					  BUFFER_SIZE,
-					  0,
-					  (struct sockaddr *) &si_outra,
-					  (socklen_t *) &slen
-				       )) == -1) {
-		  error("Erro no recvfrom");
-	  }
+	do {
+		// Espera recepção de mensagem (a chamada é bloqueante)
+		if ((receivedBytes = recvfrom(
+						socketFD,
+						buffer,
+						BUFFER_SIZE,
+						0,
+						(struct sockaddr *) &si_outra,
+						(socklen_t *) &slen
+					     )) == -1) {
+			error("Erro no recvfrom");
+		}
 
-	  // Para ignorar o restante conteúdo (anterior do buffer)
-	  buffer[receivedBytes] = '\0';
+		// Para ignorar o restante conteúdo (anterior do buffer)
+		buffer[receivedBytes] = '\0';
 
-	  int decimal = atoi(buffer);
+		int decimal = atoi(buffer);
 
-	  printf("[" RED "%s " RESET ":" CYAN "%-6hu" RESET
-			  "] Received " BLUE "%4d" RESET
-			  " bytes: %s",
-			  inet_ntop(
-				  AF_INET,
-				  &si_outra.sin_addr,
-				  ipAddressString,
-				  INET_ADDRSTRLEN
-				  ),
-			  si_outra.sin_port,
-			  receivedBytes,
-			  buffer);
-	  if (countNChar(buffer, '\n', BUFFER_SIZE) == 0) printf("\n");
+		printf("[" RED "%s " RESET ":" CYAN "%-6hu" RESET
+				"] Received " BLUE "%4d" RESET
+				" bytes: %s",
+				inet_ntop(
+					AF_INET,
+					&si_outra.sin_addr,
+					ipAddressString,
+					INET_ADDRSTRLEN
+					),
+				si_outra.sin_port,
+				receivedBytes,
+				buffer);
+		if (countNChar(buffer, '\n', BUFFER_SIZE) == 0) printf("\n");
 
-	  char msg[BUFFER_SIZE] = { [0] = '\0', [BUFFER_SIZE - 1] = '\0' };
-	  sprintf(
-			  msg,
-			  "Número em binário: 0b%b\nNúmero em hexadecimal: 0x%X\n",
-			  decimal,
-			  decimal
-		 );
+		char msg[BUFFER_SIZE] = { [0] = '\0', [BUFFER_SIZE - 1] = '\0' };
+		sprintf(
+				msg,
+				"Número em binário: 0b%b\nNúmero em hexadecimal: 0x%X\n",
+				decimal,
+				decimal
+		       );
 
-	  if (sendto(
-				  socketFD,
-				  msg,
-				  strlen(msg),
-				  0,
-				  (struct sockaddr *) &si_outra,
-				  slen
-		    ) < 0) {
-		  error("Erro no sendto");
-	  }
-  } while (strncmp(buffer, "SAIR", 5) != 0);
+		if (sendto(
+					socketFD,
+					msg,
+					strlen(msg),
+					0,
+					(struct sockaddr *) &si_outra,
+					slen
+			  ) < 0) {
+			error("Erro no sendto");
+		}
+	} while (strncmp(buffer, "SAIR", 5) != 0);
 
-  // Fecha socket e termina programa
-  close(socketFD);
-  return 0;
+	// Fecha socket e termina programa
+	close(socketFD);
+	return 0;
 }
